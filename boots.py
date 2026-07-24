1d835644ec7b08a3b03ca94d83e46b07e44dab45
wizela0-ux <wizela0@gmail.com>
2026-07-24 15:13:12 +0300

    Remove webhook from Telegram bot initialization

--- boots.py
+++ boots.py
@@ -14,6 +14,7 @@ CHANNEL_ID = "-1004333886907"  # የክፍያ ጥያቄዎች የሚላኩበት
 ADMIN_CHAT_ID = "8125688786"   # ያንተ የግል ቴሌግራም ID
 
 bot = telebot.TeleBot(BOT_TOKEN)
+bot.remove_webhook()
 
 # የ Firebase Realtime Database ዋና ሊንክህ
 FIREBASE_URL = "https://faf-earning-money-default-rtdb.firebaseio.com"
